a420915b867de1536ebce0aee844de91ba2f1837
purwar1997 <shubhampurwar511@gmail.com>
2022-12-29 22:53:55 +0530

    added DB connection function

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
 
-const config = {
-  PORT: process.env.PORT || 4000,
-  MONGODB_URL: process.env.MONGODB_URL,
-  TOKEN_SECRET: process.env.TOKEN_SECRET,
-  TOKEN_EXPIRES_IN: process.env.TOKEN_EXPIRES_IN || '2d',
-};
+// this function is an IIFE (Immediately Invoked Function Expression)
+(async () => {
+  try {
+    const res = await mongoose.connect(config.MONGODB_URL);
+    console.log(`Database connected: ${res.connection.host}`);
 
-export default config;
+    // 'on' method will listen to error event and a callback will be fired when an error occurs
+    app.on('error', err => {
+      console.log(`Error: ${err}`);
+      throw err;
+    });
+
+    app.listen(config.PORT, () =>
+      console.log(`Server is running on http://localhost:${config.PORT}`)
+    );
+  } catch (err) {
+    console.log('Database connection failed');
+    console.log(`Error: ${err.message}`);
+    // following code can be used in place of process.exit(1)
+    throw err;
+  }
+})();
