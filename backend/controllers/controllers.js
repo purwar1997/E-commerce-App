82ecfce6886348fac289ac09cc664b37b466a5ac
purwar1997 <shubhampurwar511@gmail.com>
2023-01-01 23:48:51 +0530

    added controllers for category model

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,10 +1,13 @@
 import User from '../models/user';
+import Category from '../models/category';
 import asyncHandler from '../services/asyncHandler';
 import CustomError from '../utils/customError';
 import cookieOptions from '../utils/cookieOptions';
 import mailSender from '../utils/mailSender';
 import crypto from 'crypto';
 import bcrypt from 'bcryptjs';
+
+/***** controllers for User model *****/
 
 /**
  * @SIGNUP
@@ -259,5 +262,115 @@ export const getProfile = asyncHandler(async (_req, res) => {
     success: true,
     message: 'Success in fetching user profile',
     user,
+  });
+});
+
+/***** controllers for Category model *****/
+
+/**
+ * @CREATE_CATEGORY
+ * @REQUEST_TYPE POST
+ * @route http://localhost:4000/api/createCategory
+ * @description Controller to create a category
+ * @parameters name
+ * @returns Category object
+ */
+
+export const createCategory = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    throw new CustomError('Please enter category name', 400);
+  }
+
+  const category = await Category.create({ name });
+
+  res.status(200).json({
+    success: true,
+    message: 'A new category has been created',
+    category,
+  });
+});
+
+/**
+ * @UPDATE_CATEGORY
+ * @REQUEST_TYPE PUT
+ * @route http://localhost:4000/api/updateCategory/:categoryId
+ * @description Controller to update a category
+ * @parameters name, categoryId
+ * @returns Category object
+ */
+
+export const updateCategory = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  const { categoryId } = req.params;
+
+  if (!name) {
+    throw new CustomError('Please enter category name', 400);
+  }
+
+  await Category.findByIdAndUpdate(categoryId, { name });
+  const category = await Category.findById(categoryId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Category has been updated',
+    category,
+  });
+});
+
+/**
+ * @DELETE_CATEGORY
+ * @REQUEST_TYPE DELETE
+ * @route http://localhost:4000/api/deleteCategory/:categoryId
+ * @description Controller to delete a category
+ * @parameters categoryId
+ * @returns Response object
+ */
+
+export const deleteCategory = asyncHandler(async (req, res) => {
+  await Category.findByIdAndDelete(req.params.categoryId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Category has been deleted',
+  });
+});
+
+/**
+ * @GET_CATEGORY
+ * @REQUEST_TYPE GET
+ * @route http://localhost:4000/api/getCategory/:categoryId
+ * @description Controller to fetch a category
+ * @parameters categoryId
+ * @returns Category object
+ */
+
+export const getCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.categoryId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Category has been successfully fetched',
+    category,
+  });
+});
+
+/**
+ * @GET_CATEGORIES
+ * @REQUEST_TYPE GET
+ * @route http://localhost:4000/api/getCategories
+ * @description Controller to fetch all the categories
+ * @parameters none
+ * @returns Array of category objects
+ */
+
+export const getCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find();
+
+  res.status(200).json({
+    success: true,
+    message: 'All the categories have been fetched',
+    categories,
   });
 });
